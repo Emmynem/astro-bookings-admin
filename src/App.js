f6e91cc480d91064ad95c9dba465a4c082be1beb
Emmanuel Nwoye <emmanuelnwoye5@gmail.com>
2025-03-02 01:30:03 +0100

    Add initial routing and page components and others.

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { config } from "./config";
+import useCookie from "./hooks/useCookie";
+import Layout from "./pages/Layout";
+import Access from "./pages/Access";
+import Dashboard from "./pages/Dashboard";
+import Users from "./pages/Users";
+import SignIn from "./pages/auth/SignIn";
+import Bookings from "./pages/Bookings";
+import AddUser from "./pages/AddUser";
+import EditUserDetails from "./pages/EditUserDetails";
+
+export default function App(){
+  const {cookie} = useCookie(config.key, "");
+
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path='/internal' element={<Layout />}>
+          <Route path="dashboard" element={
+            !cookie || cookie === '' || cookie === '[object Object]' ?
+              (<Navigate replace to={"/signin"} />) :
+              (<Dashboard />)
+          } />
+          <Route path="users" element={
+            !cookie || cookie === '' || cookie === '[object Object]' ?
+              (<Navigate replace to={"/signin"} />) :
+              (<Users />)
+          } />
+          <Route path="bookings" element={
+            !cookie || cookie === '' || cookie === '[object Object]' ?
+             (<Navigate replace to={"/signin"} />) :
+             (<Bookings />)
+          } />
+          <Route path="user/add" element={
+            !cookie || cookie === '' || cookie === '[object Object]' ?
+             (<Navigate replace to={"/signin"} />) :
+             (<AddUser />)
+          } />
+          <Route path="user/edit/details" element={
+            !cookie || cookie === '' || cookie === '[object Object]' ?
+              (<Navigate replace to={"/signin"} />) :
+              (<EditUserDetails />)
+          } />
+          <Route path="*" element={<Navigate replace to={"dashboard"} />} />
+        </Route>
+        <Route path='/' element={<Access />}>
+          <Route index element={<SignIn />} />
+          <Route path="signin" element={
+            cookie && cookie !== '' && cookie !== '[object Object]' ?
+              (<Navigate replace to={"/internal/dashboard"} />) :
+              (<SignIn />)
+          } />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
